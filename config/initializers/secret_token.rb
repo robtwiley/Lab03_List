53cd333e3e0e7b6fd8e0148a104eb16f33c0107a
robtwiley <robtwiley@cox.net>
2010-10-08 19:55:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab03List::Application.config.secret_token = '276dc5f41291f32face5ef8440fba8eab9d513e15f27d8de4e2e2a8186aa2c3c717733763a7df020ded56d53d118811a04efb80724915265049156a39e30d56a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lab03List::Application.config.session_store :cookie_store, :key => '_Lab03_List_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Lab03List::Application.config.session_store :active_record_store
